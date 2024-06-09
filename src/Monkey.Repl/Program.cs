9369fb450b4fc1b2f45f5c0c1a1565b86f30737e
eggerino <eggensberger.armin@googlemail.com>
2024-06-09 21:05:59 +0200

    added start of repl

--- src/Monkey.Repl/Program.cs
+++ src/Monkey.Repl/Program.cs
@@ -1,1 +1,25 @@
-﻿System.Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Monkey;
+
+Console.WriteLine($"Hello {Environment.UserName}! This is the Monkey Programming Language!");
+Console.WriteLine("Feel free to type in commands");
+
+Start(Console.In, Console.Out);
+
+static void Start(TextReader inputReader, TextWriter outputWriter)
+{
+    outputWriter.Write(">> ");
+    string? input;
+    while ((input = inputReader.ReadLine()) is not null)
+    {
+        var lexer = Lexer.For(input);
+        foreach (var token in lexer.GetTokens())
+        {
+            outputWriter.WriteLine(token);
+        }
+
+        outputWriter.Write(">> ");
+    }
+    outputWriter.WriteLine("\nExiting the REPL");
+}
