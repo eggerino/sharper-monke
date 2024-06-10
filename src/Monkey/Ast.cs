6bf60dfcb4c31d22dc1c374fd9257c3bbff693ee
eggerino <eggensberger.armin@googlemail.com>
2024-06-10 21:57:25 +0200

    add display string for debugging

--- src/Monkey/Ast.cs
+++ src/Monkey/Ast.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 
 namespace Monkey.Ast;
 
 public interface INode
 {
     string GetTokenLiteral();
+    string DisplayString();
 }
 
 public interface IStatement : INode {}
@@ -15,19 +18,60 @@ public interface IExpression : INode {}
 public record Program(ImmutableList<IStatement> Statements) : INode
 {
     public string GetTokenLiteral() => Statements.FirstOrDefault()?.GetTokenLiteral() ?? "";
+
+    public string DisplayString()
+    {
+        var builder = new StringBuilder();
+        foreach (var statement in Statements)
+        {
+            builder.Append(statement.DisplayString());
+        }
+        return builder.ToString();
+    }
 }
 
 public record LetStatement(Token Token, Identifier Name, IExpression Value) : IStatement
 {
     public string GetTokenLiteral() => Token.Literal;
+
+    public string DisplayString()
+    {
+        var builder = new StringBuilder();
+        builder.Append(GetTokenLiteral());
+        builder.Append(" ");
+        builder.Append(Name.DisplayString());
+        builder.Append(" = ");
+        builder.Append(Value.DisplayString());
+        builder.Append(";");
+        return builder.ToString();
+    }
 }
 
 public record ReturnStatement(Token Token, IExpression ReturnValue) : IStatement
 {
     public string GetTokenLiteral() => Token.Literal;
+
+    public string DisplayString()
+    {
+        var builder = new StringBuilder();
+        builder.Append(GetTokenLiteral());
+        builder.Append(" ");
+        builder.Append(ReturnValue.DisplayString());
+        builder.Append(";");
+        return builder.ToString();
+    }
+}
+
+public record ExpressionStatement(Token Token, IExpression Expression) : IStatement
+{
+    public string GetTokenLiteral() => Token.Literal;
+
+    public string DisplayString() => Expression.DisplayString();
 }
 
 public record Identifier(Token Token, string Value) : IExpression
 {
     public string GetTokenLiteral() => Token.Literal;
+
+    public string DisplayString() => Value;
 }
---
+++ test/Monkey.Test/AstTest.cs
@@ -0,0 +1,20 @@
+using Monkey.Ast;
+
+namespace Monkey.Test;
+
+public class AstTest
+{
+    [Fact]
+    public void TestDisplayString()
+    {
+        var program = new Program([
+            new LetStatement(
+                new Token(TokenType.Let, "let"),
+                new Identifier(new Token(TokenType.Identifier, "myVar"), "myVar"),
+                new Identifier(new Token(TokenType.Identifier, "anotherVar"), "anotherVar")
+            ),
+        ]);
+
+        Assert.Equal("let myVar = anotherVar;", program.DisplayString());
+    }
+}
